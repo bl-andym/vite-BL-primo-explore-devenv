3bb308446f69e71430b2cbfaf400fa5539d732ce
andy <andrew.marsden@bl.uk>
2023-08-08 12:30:53 +0100

    subtract

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,28 @@ import './App.css'
 
 function Count() {
   const [count, setCount] = useState(0);
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount((count) => count - 1);
+    }
+  };
+
   return (
-    <p>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </p>
-  )
+    <div>
+      <p>
+        <button onClick={() => setCount((count) => count + 1)}>
+          Add: {count}
+        </button>
+        {count > 0 && (
+          <button onClick={handleDecrement}>
+            Subtract:
+          </button>
+        )}
+      </p>
+
+    </div>
+  );
 }
 
 function App() {
